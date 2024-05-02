36383c7d7abde6ac823be2cd530ccf9378ca3047
Abubakar Aamir <153678759+AbubakarAamir@users.noreply.github.com>
2024-05-02 13:47:39 -0700

    Add files via upload

---
+++ LAB 1.C
@@ -0,0 +1,102 @@
+   //Made by :Abubakar Aamir .
+
+
+
+//              LAB 1 : PRACTISE C QUESTIONS :
+
+//             Q1: Write a program to beep five times.
+#include<stdio.h>
+#include<stdlib.h> //This specific header file is used to produce sound beeps .
+int main(){
+
+printf("\a \a \a \a \a "); //(/a) this is a format specifcier used in c to produce siound beeps .  
+
+    return 0;
+}
+
+
+
+
+//output :
+//produces sound five times 
+
+
+
+
+//           Q2: What does the following code prints ?
+
+#include<stdio.h> 
+int main(){
+
+printf("*\n");
+printf("**\n");
+printf("\n****");
+    return 0;
+}
+
+// output:
+// *
+// **
+// ****
+
+  //       Q3: Write a c program to print the following shapes using escape sequences?
+
+#include<stdio.h>
+int main(){
+
+printf("*\n");
+printf("* *\n");
+printf("* * * \n");
+printf("* * * * ");
+
+printf("\n \n \n");
+printf("*\n");
+printf("* *\n");
+printf("* * *\n");
+printf("* *\n");
+printf("*\n");
+
+    return 0;
+}
+
+  //output:
+
+  //*
+  //* *
+  //* * *
+  //* * * *
+   
+
+   // *
+   // * *
+   // * *  * 
+   // * *
+   // *
+
+
+   //         Q4:Write a program that prints the folowing output using  asterisks?
+  
+  #include<stdio.h>
+  int main(){
+    printf("*********    ***         *               *   \n");
+    printf("*       *  *     *     * * *           *    *\n");
+    printf("*       *  *     *       *            *       *\n");
+    printf("*       *  *     *       *           *          *\n");
+    printf("*       *  *     *       *          *          *\n");
+    printf("*       *     *          *            *        \n");
+    printf("********                 *                *\n");
+    return 0;
+  }
+
+
+
+
+
+   //output:
+   //*********     ***          *          * 
+   //*       *   *     *      * * *      *   *
+   //*       *   *     *        *       *     *
+   //*       *   *     *        *       *     *
+   //*       *   *     *        *       *     *
+   //*       *   *     *        *       *     *
+   //* * * * *      *           *          *
